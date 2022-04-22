2289a68de8264da39bab7fc6dcf0fdf400c9e804
tumtpk <pk.prateep@gmail.com>
2022-04-22 15:36:13 +0700

    add header banner layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Header from './components/Header';
+import Banner from './components/Banner';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <Router>
+
+        <Header/>
+        <Banner/>
+
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    )
+  }
 }
+
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+
+class Header extends React.Component{
+    render(){
+        return(
+            <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+                <div className="container">
+                <a className="navbar-brand" href="index.html">Kusina</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="oi oi-menu"></span> Menu
+                </button>
+
+                <div className="collapse navbar-collapse" id="ftco-nav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active"><a href="index.html" className="nav-link">Home</a></li>
+                        <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
+                        <li className="nav-item"><a href="menu.html" className="nav-link">Specialties</a></li>
+                        <li className="nav-item"><a href="reservation.html" className="nav-link">Reservation</a></li>
+                        <li className="nav-item"><a href="blog.html" className="nav-link">Stories</a></li>
+                    <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header;

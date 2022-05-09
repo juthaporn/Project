c22c46e00ac9169daca088a149739d887bf39054
sarinya kongseng <59682762+zsarinya10z@users.noreply.github.com>
2022-05-09 13:34:39 +0700

    page

--- frontend/src/pages/OrderOwner.js
+++ frontend/src/pages/OrderOwner.js
@@ -16,54 +16,127 @@ class OrderOwner extends React.Component{
 
     render(){
         return(   
-                   
-            <div class ="container">    
-            <div class="bradcam_area bradcam_bg_1">
-                  <div class="container">
-                      <div class="row">
-                          <div class="col-xl-12">
-                              <div class="bradcam_text text-center">
-                                  <h3>order</h3>
-                              </div>
-                          </div>
-                      </div>
+            <div class ="container">
+                <br/>
+            <div class="col-md-12 grid-margin stretch-card">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-title mb-0">สถานะการสั่งซื้อ</p>
+                <div class="table-responsive">
+                  <table class="table table-striped table-borderless">
+                    <thead>
+                      <tr>
+                        <th>ลำดับ</th>
+                        <th>รายการ</th>
+                        <th>เวลา</th>
+                        <th>สถานะ</th>
+                        <th>แก้ไข</th>
+
+                      </tr>  
+                    </thead>
+                    <tbody>
+                      <tr>
+                      <td class="font-weight-bold">1</td>
+                        <td>Search Engine Marketing</td>
+  
+                        <td>20:18</td>
+                        <td class="font-weight-medium"><div class="badge badge-success">เสร็จสิ้น</div></td>
+                        <td class="font-weight-medium"><div class="badge badge-danger">แก้ไข</div></td>
+                      </tr>
+                      <tr>
+                      <td class="font-weight-bold">2</td>
+                        <td>Search Engine Optimization</td>
+                       
+                        <td>20:18</td>
+                        <td class="font-weight-medium"><div class="badge badge-success">เสร็จสิ้น</div></td>
+                        <td class="font-weight-medium"><div class="badge badge-danger">แก้ไข</div></td>
+                      </tr>
+                      <tr>
+                      <td class="font-weight-bold">3</td>
+                        <td>Display Advertising</td>
+                        
+                        <td>20:18</td>
+                        <td class="font-weight-medium"><div class="badge badge-warning">กำลังปรุง</div></td>
+                        <td class="font-weight-medium"><div class="badge badge-danger">แก้ไข</div></td>
+                      </tr>
+                      <tr>
+                      <td class="font-weight-bold">4</td>
+                        <td>Pay Per Click Advertising</td>
+                       
+                        <td>20:18</td>
+                        <td class="font-weight-medium"><div class="badge badge-warning">กำลังปรุง</div></td>
+                        <td class="font-weight-medium"><div class="badge badge-danger">แก้ไข</div></td>
+                      </tr>
+                      <tr>
+                      <td class="font-weight-bold">5</td>
+                        <td>E-Mail Marketing</td>
+                        
+                        <td>20:18</td>
+                        <td class="font-weight-medium"><div class="badge badge-danger">ยกเลิก</div></td>
+                        <td class="font-weight-medium"><div class="badge badge-danger">แก้ไข</div></td>
+                      </tr>
+                      <tr>
+                      <td class="font-weight-bold">6</td>
+                        <td>Referral Marketing</td>
+                        
+                        <td>20:18</td>
+                        <td class="font-weight-medium"><div class="badge badge-primary">รอ</div></td>
+                        <td class="font-weight-medium"><div class="badge badge-danger">แก้ไข</div></td>
+                      </tr>
+                      <tr>
+                      <td class="font-weight-bold">7</td>
+                        <td>Social media marketing</td>
+                     
+                        <td>10:18</td>
+                        <td class="font-weight-medium"><div class="badge badge-success">เสร็จสิ้น</div></td>
+                        <td class="font-weight-medium"><div class="badge badge-danger">แก้ไข</div></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br/>
+          <div class="row">
+                <div class="col-md-6 mb-4 stretch-card transparent">
+                  <div class="card card-tale">
+                    <div class="card-body">
+                    <td class="font-weight-medium"><div class="badge badge-primary">รายการที่รอคิว</div></td>
+                      <p class="mb-4">วันที่ </p>
+                      <p>รายการที่เสร็จสมบูรณ์ทั้งหมด 25 รายการ</p>
+                    </div>
                   </div>
-            </div>
-                <div class="card-body">
-                    <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                    <th scope="col">id </th>
-                                    <th scope="col">productName</th>
-                                    <th scope="col">detail</th>
-                                    <th scope="col">order date</th>
-                                    <th scope="col">order status</th>
-                                    <th scope="col">subtotal</th>
-                                    <th scope="col">orderDetail</th>
-                                    <th scope="col">price</th>
-                                    <th scope="col">Quantity</th> 
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>Mark</td>
-                                    
-                                    
-                                    </tr>
-                                </tbody>
-                                </table>
-                        </div>
-            </div>
-            
-       
+                </div>
+                <div class="col-md-6 mb-4 stretch-card transparent">
+                  <div class="card card-dark-blue">
+                    <div class="card-body">
+                    <td class="font-weight-medium"><div class="badge badge-warning">รายการที่กำลังปรุง</div></td>
+                      <p class="mb-4">วันที่ </p>
+                      <p>รายการที่ยกเลิกทั้งหมด 25 รายการ</p>
+                    </div>
+                  </div>
+                </div>
+                 <div class="col-md-6 mb-4 stretch-card transparent">
+                  <div class="card card-tale">
+                    <div class="card-body">
+                    <td class="font-weight-medium"><div class="badge badge-success">รายการที่เสร็จสมบูรณ์</div></td>
+                      <p class="mb-4">วันที่ </p>
+                      <p>รายการที่เสร็จสมบูรณ์ทั้งหมด 25 รายการ</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 mb-4 stretch-card transparent">
+                  <div class="card card-dark-blue">
+                    <div class="card-body">
+                    <td class="font-weight-medium"><div class="badge badge-danger">รายการที่ยกเลิกแล้ว</div></td>
+                      <p class="mb-4">วันที่ </p>
+                      <p>รายการที่ยกเลิกทั้งหมด 25 รายการ</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
         )
     }
 }
